ed17375ed6b2560358f5482991980660db40be30
CuongNguyen13 <vancuong13032000@gmail.com>
2021-05-21 13:12:32 +0700

    chình sửa giao diện menu

--- app/src/main/java/com/example/laptrinhandroidnhom6/list_order/OrderFagment.java
+++ app/src/main/java/com/example/laptrinhandroidnhom6/list_order/OrderFagment.java
@@ -1,28 +1,22 @@
-package com.example.laptrinhandroidnhom6;
+package com.example.laptrinhandroidnhom6.list_order;
 
 import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TableLayout;
 
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentStatePagerAdapter;
-import androidx.viewpager.widget.ViewPager;
-
-import com.example.laptrinhandroidnhom6.list_order.OrderFagment;
-import com.example.laptrinhandroidnhom6.list_order.PagerAdepterOrder;
-import com.google.android.material.tabs.TabLayout;
+import com.example.laptrinhandroidnhom6.R;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link ListOrder_Fragment#newInstance} factory method to
+ * Use the {@link OrderFagment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class ListOrder_Fragment extends Fragment {
-    private TabLayout tabLayout;
-    private ViewPager viewPager;
-    private View view;
+public class OrderFagment extends Fragment {
+
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -32,7 +26,7 @@ public class ListOrder_Fragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public ListOrder_Fragment() {
+    public OrderFagment() {
         // Required empty public constructor
     }
 
@@ -42,11 +36,11 @@ public class ListOrder_Fragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment ListOrder_Fragment.
+     * @return A new instance of fragment OrderFagment.
      */
     // TODO: Rename and change types and number of parameters
-    public static ListOrder_Fragment newInstance(String param1, String param2) {
-        ListOrder_Fragment fragment = new ListOrder_Fragment();
+    public static OrderFagment newInstance(String param1, String param2) {
+        OrderFagment fragment = new OrderFagment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -67,12 +61,6 @@ public class ListOrder_Fragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-      view = inflater.inflate(R.layout.fragment_list_order_, container, false);
-        tabLayout = view.findViewById(R.id.taborderList);
-        viewPager = view.findViewById(R.id.listOrderPager);
-        PagerAdepterOrder adepterOrder = new PagerAdepterOrder(getChildFragmentManager(), FragmentStatePagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
-        viewPager.setAdapter(adepterOrder);
-        tabLayout.setupWithViewPager(viewPager);
-        return view;
+        return inflater.inflate(R.layout.fragment_order_fagment, container, false);
     }
 }
