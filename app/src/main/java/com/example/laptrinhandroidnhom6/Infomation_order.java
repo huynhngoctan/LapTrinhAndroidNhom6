035c599e1a6075d6370c7245bf2e288805c1bfc2
CuongNguyen13 <vancuong13032000@gmail.com>
2021-06-17 22:11:53 +0700

    thông tin đặt hàng

--- app/src/main/java/com/example/laptrinhandroidnhom6/Infomation_order.java
+++ app/src/main/java/com/example/laptrinhandroidnhom6/Infomation_order.java
@@ -7,13 +7,20 @@ import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 
+import com.example.laptrinhandroidnhom6.custom.Custom_Adapter_list_infor;
+import com.example.laptrinhandroidnhom6.custom.Custom_Cart;
 import com.example.laptrinhandroidnhom6.map.MyMapFragment;
+import com.example.laptrinhandroidnhom6.model.ItemOfListOrder;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
@@ -21,15 +28,37 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.util.ArrayList;
+
 public class Infomation_order extends Activity implements OnMapReadyCallback {
     Button btnBack;
-
+    ArrayList<ItemOfListOrder> listItem;
+    TextView txtPrice;
+    RadioGroup radioGroup;
+    RadioButton raBtnCod,raBtnOnline;
     @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_information_order);
+        createListItemOrder();
 //        btnBack = findViewById(R.id.btnReturn);
+        //Tinh tong gia
+        txtPrice = findViewById(R.id.sumPrice);
+
+        txtPrice.setText(""+sumPrice());
+        //thanh toan
+
+
+
+
+
+        //list food
+        ListView listView = findViewById(R.id.listViewOrder);
+
+        Custom_Adapter_list_infor adapter = new Custom_Adapter_list_infor(listItem, this);
+        listView.setAdapter(adapter);
+
         //map
 
         MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.mymap);
@@ -52,5 +81,25 @@ public class Infomation_order extends Activity implements OnMapReadyCallback {
         .snippet("cuong123")
                 .position(syd)
         );
+    }
+    public void createListItemOrder() {
+        listItem = new ArrayList<>();
+        listItem.add(new ItemOfListOrder(1, "ăn", "https://img.wattpad.com/cover/123681914-256-k224486.jpg", "SG", 200, "20-02-2021"));
+        listItem.add(new ItemOfListOrder(1, "ăn", "https://img.wattpad.com/cover/123681914-256-k224486.jpg", "SG", 200, "20-02-2021"));
+        listItem.add(new ItemOfListOrder(1, "ăn", "https://img.wattpad.com/cover/123681914-256-k224486.jpg", "SG", 200, "20-02-2021"));
+        listItem.add(new ItemOfListOrder(1, "ăn", "https://img.wattpad.com/cover/123681914-256-k224486.jpg", "SG", 200, "20-02-2021"));
+        listItem.add(new ItemOfListOrder(1, "ăn", "https://img.wattpad.com/cover/123681914-256-k224486.jpg", "SG", 200, "20-02-2021"));
+        listItem.add(new ItemOfListOrder(1, "ăn", "https://img.wattpad.com/cover/123681914-256-k224486.jpg", "SG", 200, "20-02-2021"));
+        listItem.add(new ItemOfListOrder(1, "ăn", "https://img.wattpad.com/cover/123681914-256-k224486.jpg", "SG", 200, "20-02-2021"));
+
+    }
+
+    //tinh tong tien
+    public int sumPrice(){
+        int sum =0;
+        for (int i=0 ;i<5;i++){
+            sum += listItem.get(i).getPrice();
+        }
+        return sum;
     }
 }
---
+++ app/src/main/java/com/example/laptrinhandroidnhom6/custom/Custom_Adapter_list_infor.java
@@ -0,0 +1,63 @@
+package com.example.laptrinhandroidnhom6.custom;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.laptrinhandroidnhom6.R;
+import com.example.laptrinhandroidnhom6.load_image_internet.LoadImage;
+import com.example.laptrinhandroidnhom6.model.ItemOfListOrder;
+
+import java.util.ArrayList;
+
+public class Custom_Adapter_list_infor extends BaseAdapter {
+    private ArrayList<ItemOfListOrder> listOrder;
+    private Activity activity;
+
+    public Custom_Adapter_list_infor(ArrayList<ItemOfListOrder> listOrder, Activity activity) {
+        this.listOrder = listOrder;
+        this.activity = activity;
+    }
+
+
+    @Override
+    public int getCount() {
+        return listOrder.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return listOrder.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = activity.getLayoutInflater();
+        convertView = inflater.inflate(R.layout.item_order_confirm, null);
+        TextView name,price;
+        ImageView img;
+
+        img = convertView.findViewById(R.id.img);
+        name = convertView.findViewById(R.id.nameFood);
+        price = convertView.findViewById(R.id.price);
+
+        new LoadImage(img).execute(listOrder.get(position).getImg());
+        name.setText(listOrder.get(position).getName());
+        price.setText(listOrder.get(position).getPrice()+"");
+
+
+
+        return convertView;
+    }
+
+}
